68fec90510153731426318f4f7baf27263c153fe
Ninian Bond III <nubond3@gmail.com>
2020-10-12 20:18:48 -0400

    added functional solution for manual_exponent

--- manual_exponent/main.py
+++ manual_exponent/main.py
@@ -1,12 +1,24 @@
+# def manual_exponent(base, exp):
+#     total = 1 
+#     index = 1
+
+#     while index <= exp:
+#         total *= base
+#         index += 1 
+
+#     return total 
+
+import functools
+
+
 def manual_exponent(base, exp):
-    total = 1 
-    index = 1
+    numbers = [base for y in range(exp)]
 
-    while index <= exp:
-        total *= base
-        index += 1 
+    return functools.reduce(lambda x, y: x * y, numbers)
 
-    return total 
+
+
 
 
 print(manual_exponent(5, 5))
+
