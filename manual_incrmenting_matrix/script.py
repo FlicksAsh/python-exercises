4c6f5cd98902d9389c0c5f7bd9a185f75d94cca9
Ninian Bond III <nubond3@gmail.com>
2020-10-13 12:27:47 -0400

    refactored my solution for script.py

--- manual_incrmenting_matrix/script.py
+++ manual_incrmenting_matrix/script.py
@@ -1,13 +1,11 @@
 def manual_incrementing_matrix(d):
   rows = [] 
   x = 0
-  for y in range(d + x):
-    y += x
+  for y in range(d):
     row = [] 
     for z in range(d):
       z += x 
       row.append(z)
-
     x += 1 
     rows.append(row)
     print(row)
