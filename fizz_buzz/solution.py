7ce3eca6e93f5a3571ddd09ef6cbf23bdb0706fe
Ninian Bond III <nubond3@gmail.com>
2020-10-18 20:03:35 -0400

    added doc string to fizz_buzz

--- fizz_buzz/solution.py
+++ fizz_buzz/solution.py
@@ -1,3 +1,14 @@
+"""
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print “Fizz” instead of the
+number and for the multiples of five print “Buzz”. For
+numbers which are multiples of both three and five print
+“FizzBuzz”.
+"""
+
+
+
+
 def fizz_buzz(max_value):
     for num in range(1, max_value + 1):
         if num % 5 == 0 and num % 3 == 0:
